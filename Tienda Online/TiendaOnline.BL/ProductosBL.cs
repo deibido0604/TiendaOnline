d6e8bba6526ba1511502b9eb806f04ff20e1c10c
DAVID JARED TORRES ROMERO <jared.torres@unah.hn>
2021-12-15 22:30:04 -0600

    Video 8

---
+++ Tienda Online/TiendaOnline.BL/ProductosBL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaOnline.BL
+{
+    public class ProductosBL
+    {
+        public List<Producto> ObtenerProductos()
+        {
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "Retrato";
+            producto1.Precio = 1400;
+
+            var producto2 = new Producto();
+            producto2.Id = 1;
+            producto2.Descripcion = "Paisaje";
+
+            var producto3 = new Producto();
+            producto3.Id = 1;
+            producto3.Descripcion = "Animal";
+
+            var listaDeProductos = new List<Producto>();
+            listaDeProductos.Add(producto1);
+            listaDeProductos.Add(producto2);
+            listaDeProductos.Add(producto3);
+
+            return listaDeProductos;
+        }
+    }
+}
--- Tienda Online/Web/Controllers/ProductoController.cs
+++ Tienda Online/Web/Controllers/ProductoController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Web.Models;
+using TiendaOnline.BL;
 
 namespace Web.Controllers
 {
@@ -12,22 +12,8 @@ namespace Web.Controllers
         // GET: Producto
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Retrato";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 1;
-            producto2.Descripcion = "Paisaje";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 1;
-            producto3.Descripcion = "Animal";
-
-            var listaDeProductos = new List<ProductoModel>();
-            listaDeProductos.Add(producto1);
-            listaDeProductos.Add(producto2);
-            listaDeProductos.Add(producto3);
+            var productosBL = new ProductosBL();
+            var listaDeProductos = productosBL.ObtenerProductos();
 
             return View(listaDeProductos);
         }
