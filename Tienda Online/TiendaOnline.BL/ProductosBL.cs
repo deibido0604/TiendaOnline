37cb12886a56891a66682504778bbaa1bf356b66
DAVID JARED TORRES ROMERO <jared.torres@unah.hn>
2021-12-18 23:01:14 -0600

    Video 13

--- Tienda Online/TiendaOnline.BL/ProductosBL.cs
+++ Tienda Online/TiendaOnline.BL/ProductosBL.cs
@@ -8,27 +8,15 @@ namespace TiendaOnline.BL
 {
     public class ProductosBL
     {
+        Contexto _contexto;
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+        }
         public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Retrato";
-            producto1.Precio = 1400;
 
-            var producto2 = new Producto();
-            producto2.Id = 1;
-            producto2.Descripcion = "Paisaje";
-
-            var producto3 = new Producto();
-            producto3.Id = 1;
-            producto3.Descripcion = "Animal";
-
-            var listaDeProductos = new List<Producto>();
-            listaDeProductos.Add(producto1);
-            listaDeProductos.Add(producto2);
-            listaDeProductos.Add(producto3);
-
-            return listaDeProductos;
+            return _contexto.Productos.ToList();
         }
     }
 }
